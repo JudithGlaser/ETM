7a0ae435bcc78e243ad376de9b32aa77e3935f9d
maerzhase <markus@lili16.de>
2021-02-03 17:48:12 +0100

    add naive approach for second poti

--- step-res.py
+++ step-res.py
@@ -14,18 +14,21 @@ import paho.mqtt.publish as publish
 GPIO.setmode(GPIO.BCM)
 
 # define GPIO pins with variables a_pin and b_pin
-a_pin = 18
-b_pin = 23
+a_pin1 = 18
+b_pin1 = 23
+
+a_pin2 = 18
+b_pin2 = 23
 
 # create discharge function for reading capacitor data
-def discharge():
+def discharge1():
     GPIO.setup(a_pin, GPIO.IN)
     GPIO.setup(b_pin, GPIO.OUT)
     GPIO.output(b_pin, False)
     time.sleep(0.005)
 
 # create time function for capturing analog count value
-def charge_time():
+def charge_time1():
     GPIO.setup(b_pin, GPIO.IN)
     GPIO.setup(a_pin, GPIO.OUT)
     count = 0
@@ -35,12 +38,36 @@ def charge_time():
     return count
 
 # create analog read function for reading charging and discharging data
-def analog_read():
-    discharge()
-    return charge_time()
+def analog_read1():
+    discharge1()
+    return charge_time1()
+
+# create discharge function for reading capacitor data
+def discharge2():
+    GPIO.setup(a_pin2, GPIO.IN)
+    GPIO.setup(b_pin2, GPIO.OUT)
+    GPIO.output(b_pin2, False)
+    time.sleep(0.005)
+
+# create time function for capturing analog count value
+def charge_time2():
+    GPIO.setup(b_pin2, GPIO.IN)
+    GPIO.setup(a_pin2, GPIO.OUT)
+    count = 0
+    GPIO.output(a_pin2, True)
+    while not GPIO.input(b_pin2):
+        count = count +1
+    return count
+
+# create analog read function for reading charging and discharging data
+def analog_read2():
+    discharge2()
+    return charge_time2()
 
 # provide a loop to display analog data count value on the screen
 while True:
-    value = analog_read()
-    publish.single("a0", value, hostname="localhost")
+    value1 = analog_read1()
+    value2 = analog_read2()
+    publish.single("a0", value1, hostname="localhost")
+    publish.single("a1", value2, hostname="localhost")
     time.sleep(0.016)
